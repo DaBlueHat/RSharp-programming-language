ca16ff529337fa557006b441914f18186ccffa32
DaBlueHat <97629478+DaBlueHat@users.noreply.github.com>
2022-02-12 11:09:52 +0000

    Rsharp

---
+++ Rsharp/__init__.py
@@ -0,0 +1,21 @@
+import random
+import os
+
+def println(string):
+    print(string)
+
+def add_nums(num1, num2):
+    print(num1 + num2)
+
+def subtract_nums(subnum1, subnum2):
+    print(subnum1 - subnum2)
+
+def divide_nums(divnum1, divnum2):
+    print(divnum1 / divnum2)
+
+def printrand(min, max):
+    rand = random.randint(min, max)
+    print(rand)
+
+def startfile(filename):
+    os.startfile(filename)
